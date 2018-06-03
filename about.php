38b068d4515f4a07fd7bbe638221eabb372b43b2
Royclassic <raymalaba@gmail.com>
2018-06-03 04:21:28 +0300

    student_security

---
+++ about.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Student Security</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+  </head>
+
+  <body>
+
+    <!-- ------------ HEADER -------------- -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-default fixed-top">
+      <a class="navbar-brand" href="index.php"><img src="images/bower-logo.png" width="52px"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About Us</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact us</a>
+          </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" style="cursor: hand" aria-expanded="false">School</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="admin.php">admin</a>
+                   <a class="dropdown-item" href="teacher.php">teacher</a>
+                </div>
+              </li>
+            </ul>
+          <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2 " type="text" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-danger my-2 my-sm-0 hideme" type="submit"><i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </form>
+    </nav>
+    <!-- end of navbar -->
+    <!-- END OF HEADER -->
+    <!-- ---------------- ABOUT US ------------------ -->
+    <div class="about_us" id="about_us">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-sm-12 col-md-12 wow fadeInLeft packages" data-wow-delay="1s">
+            <center><h2>History</h2></center><br>
+          </div><!-- end of col -->
+        </div><!-- end of row -->
+        <div class="row">
+          <div class="col-sm-6 col-md-6 col-lg-6">
+            <div class="w3-content w3-section" style="max-width:100%">
+              <!-- <img class="mySlides w3-animate-fading" src="images/Burundi.png" style="width:100%; height: 270px; margin-top: 10px;"> -->
+              <img class="mySlides w3-animate-fading" src="images/nairobi.jpg" style="width:100%; height: 280px; margin-top: 10px;">
+              <img class="mySlides w3-animate-fading" src="images/tourist.jpg" style="width:100%; height: 270px; margin-top: 10px;">
+              <img class="mySlides w3-animate-fading" src="images/merry-christmas.jpg" style="width:100%; height: 280px; margin-top: 10px;">
+            </div>
+          </div><!-- end of col -->
+
+          <div class="col-sm-6 col-md-6 col-lg-6 wow fadeInRight packages" data-wow-delay="1.6s">
+
+            <p>
+              This is a group of three members: ROY,STEVE and MAXIME (Trio-Coders) studying at the Catholic Universty of Eastern Africa who sat down and so the need of people having access to their constitution and other countries constitution. This idea came into our mind after seeing most of the foreign students or people struggling in a particular counry simply because they aren't familiar with that country's constitution. So we so the need of providing for them this web site to enable them be able to read and be familiar with the country's rules and regulations.
+            </p>
+          </div><!-- end of col -->
+        </div><!-- end of row -->
+
+        <br><br><div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12 wow fadeInLeft packages" data-wow-delay="3s"">
+            <center><h2>Vision and Objectives</h2></center><br>
+            <p>
+              
+              Our mission is to try and make sure that all people with any kind of disability are able to use this web site because most of the web developers do not put the interests of the needy when developing their web sites.
+              Our objectives include;
+                    Collecting the constitutional documentation of different countris.
+                    Collecting more views from both the locals and internationals on how they would like the web site to look like.
+                    Trying to see all kind of disability most of the peole have and be able to meet them in our web site.
+                    Comparing our web site with others to see the kind features ommitted and try to include in ours. 
+
+
+              </p>
+          </div><!-- end of col -->
+        </div><!-- end of row -->
+
+        <div class="video" id="video">
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 col-md-12">
+              <center><h2>Videos</h2></center><br>
+            </div><!-- end of col -->
+          </div><!-- end of row -->
+
+          <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+              <center><h2>History</h2></center>
+              <video width="100%" controls>
+                <source src="videos/Risk_and_reward.mp4" type="video/mp4">
+                <source src="videos/Risk_and_reward.mp4" type="video/ogg">
+                Your browser does not support HTML5 video.
+              </video>
+            </div><!-- end of col -->
+
+            <div class="col-sm-6 col-md-6 col-lg-6">
+              <center><h2>Vision and Objective</h2></center>
+              <video width="100%" controls>
+                <source src="videos/Risk_and_reward.mp4" type="video/mp4">
+                <source src="videos/Risk_and_reward.mp4" type="video/ogg">
+                Your browser does not support HTML5 video.
+              </video>
+            </div><!-- end of col -->
+
+          </div><!-- end of row -->
+        </div><!-- end of trasted_by -->
+      </div><!-- end of container -->
+    </div><br><br><!-- end of about_us -->
+
+
+
+         
+   <!-- -------------------- footer -------------------->
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div class="contact_info"> 
+              <h4>Contact us</h4>
+              <ul>
+              <i class="fa fa-phone" aria-hidden="true"></i> 0700000000<br>
+              <i class="fa fa-envelope-o" aria-hidden="true"></i> 3s@gmail.com<br>
+              <i class="fa fa-home" aria-hidden="true"></i> Karen, Nairobi-Kenya<br>
+              </ul>
+            </div>
+          </div><!-- end of col -->
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div> 
+              <h4>About us</h4>
+              <ul>
+                <i class="fa fa-square-o" aria-hidden="true"></i> Service<br>
+                <i class="fa fa-square-o" aria-hidden="true"></i> About Us<br>
+                <i class="fa fa-square-o" aria-hidden="true"></i> Term & Condition<br>
+              </ul>
+            </div>
+          </div><!-- end of col -->
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div> 
+              <h4>Stay in touch</h4>
+              <i class="social fa fa-facebook-official facebook" aria-hidden="true"></i>
+              <i class="fa fa-twitter twitter" aria-hidden="true"></i>
+              <i class="fa fa-linkedin linkedin" aria-hidden="true"></i></i>
+              <i class="fa fa-pinterest-p pinterest" aria-hidden="true"></i>
+              <div class="newsletter">
+                <form>
+                  <div class="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <button type="submit" class="btn btn-danger">Submit</button>
+                </form>
+              </div>
+
+            </div>
+          </div> <!-- end of col -->
+        </div><br>
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            ©2018. All Rights Reserved
+          </div>
+        </div><!-- end of row -->
+      </div>
+    </div>
+ 
+    <!-- end of the footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ school.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Student Security</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+  </head>
+
+  <body>
+
+    <!-- ------------ HEADER -------------- -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-default fixed-top">
+      <a class="navbar-brand" href="index.php"><img src="images/bower-logo.png" width="52px"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About Us</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact us</a>
+          </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" style="cursor: hand" aria-expanded="false">School</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="admin.php">admin</a>
+                   <a class="dropdown-item" href="teacher.php">teacher</a>
+                </div>
+              </li>
+            </ul>
+          <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2 " type="text" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-danger my-2 my-sm-0 hideme" type="submit"><i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </form>
+    </nav><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <!-- end of navbar -->
+    <!-- END OF HEADER -->
+
+
+         
+   <!-- -------------------- footer -------------------->
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div class="contact_info"> 
+              <h4>Contact us</h4>
+              <ul>
+              <i class="fa fa-phone" aria-hidden="true"></i> 0700000000<br>
+              <i class="fa fa-envelope-o" aria-hidden="true"></i> 3s@gmail.com<br>
+              <i class="fa fa-home" aria-hidden="true"></i> Karen, Nairobi-Kenya<br>
+              </ul>
+            </div>
+          </div><!-- end of col -->
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div> 
+              <h4>About us</h4>
+              <ul>
+                <i class="fa fa-square-o" aria-hidden="true"></i> Service<br>
+                <i class="fa fa-square-o" aria-hidden="true"></i> About Us<br>
+                <i class="fa fa-square-o" aria-hidden="true"></i> Term & Condition<br>
+              </ul>
+            </div>
+          </div><!-- end of col -->
+          <div class="col-sm-4 col-md-4 col-lg-4">
+            <div> 
+              <h4>Stay in touch</h4>
+              <i class="social fa fa-facebook-official facebook" aria-hidden="true"></i>
+              <i class="fa fa-twitter twitter" aria-hidden="true"></i>
+              <i class="fa fa-linkedin linkedin" aria-hidden="true"></i></i>
+              <i class="fa fa-pinterest-p pinterest" aria-hidden="true"></i>
+              <div class="newsletter">
+                <form>
+                  <div class="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <button type="submit" class="btn btn-danger">Submit</button>
+                </form>
+              </div>
+
+            </div>
+          </div> <!-- end of col -->
+        </div><br>
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            ©2018. All Rights Reserved
+          </div>
+        </div><!-- end of row -->
+      </div>
+    </div>
+ 
+    <!-- end of the footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
